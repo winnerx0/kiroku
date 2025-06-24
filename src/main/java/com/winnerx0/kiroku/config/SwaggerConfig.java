7d28e9123fdd05dca1f65797d43d547c368cd782
winnerx0 <eragbonikpeya@gmail.com>
2025-06-24 13:06:55 +0100

    update docs

--- src/main/java/com/winnerx0/kiroku/config/SwaggerConfig.java
+++ src/main/java/com/winnerx0/kiroku/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.winnerx0.kiroku.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,15 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Kiroku")
                         .version("1.0")
-                        .description("A self made social media platform"));
+                        .description("A self made social media platform"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .in(SecurityScheme.In.HEADER)
+                                .type(SecurityScheme.Type.HTTP)
+                                .bearerFormat("JWT")
+                                .scheme("bearer")));
     }
 }
